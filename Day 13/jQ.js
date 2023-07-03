babd131cc971eaee1c4be9a5b0a3ca4333e3f550
madhavikoladiya <137100982+madhavikoladiya@users.noreply.github.com>
2023-07-03 10:11:21 -0700

    Add files via upload

---
+++ Day 13/jQ.js
@@ -0,0 +1,6 @@
+function fade()
+{
+    //$(selector).action();
+    $("#img2").fadeToggle();
+    //$("#img2").fadeToggle(3000);
+}
---
+++ Day 13/jQuery.js
@@ -0,0 +1,6 @@
+
+
+function func1() {
+    $("#div2").css("background-color", "red");
+    $("#div1,#div3").css("background-color", "yellow");
+  }
