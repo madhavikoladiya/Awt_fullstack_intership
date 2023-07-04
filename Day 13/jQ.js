524593aaf67405d143360d0643e6812834161f5b
madhavikoladiya <137100982+madhavikoladiya@users.noreply.github.com>
2023-07-04 09:29:46 -0700

    Delete Day 13 directory

--- Day 13/jQ.js
+++
@@ -1,6 +0,0 @@
-function fade()
-{
-    //$(selector).action();
-    $("#img2").fadeToggle();
-    //$("#img2").fadeToggle(3000);
-}
--- Day 13/jQuery.js
+++
@@ -1,6 +0,0 @@
-
-
-function func1() {
-    $("#div2").css("background-color", "red");
-    $("#div1,#div3").css("background-color", "yellow");
-  }
